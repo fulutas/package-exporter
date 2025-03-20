2904a153a14122aa9bb9ac13c094014457f71937
furkan.ulutas <furkan.ulutas@next4biz.com>
2025-03-21 01:30:25 +0300

    update exe.

--- getLatestVersion.js
+++ getLatestVersion.js
@@ -1,10 +1,13 @@
-const axios = require('axios');
 async function getLatestVersion(packageName, projectName) {
   console.log(`🔍 Fetching latest version package for: ${packageName} Project Name: ${projectName}`);
 
   try {
-    const response = await axios.get(`https://registry.npmjs.org/${packageName}`);
-    return response.data['dist-tags'].latest;
+    const response = await fetch(`https://registry.npmjs.org/${packageName}`);
+    if (!response.ok) {
+      throw new Error(`Error fetching version for ${packageName}`);
+    }
+    const data = await response.json();
+    return data['dist-tags'].latest;
   } catch (error) {
     console.error(`❌ Error fetching version for ${packageName}:`, error);
     return 'N/A';
